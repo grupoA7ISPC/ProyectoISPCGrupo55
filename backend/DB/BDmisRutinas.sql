da03eb012768b3588c10e057495c4499a8fae1b4
Andrés Luna <andresluna007@gmail.com>
2022-10-29 17:00:55 -0300

    DDL Terminado

--- backend/DB/BDmisRutinas.sql
+++ backend/DB/BDmisRutinas.sql
@@ -1,4 +1,3 @@
-/*Vero*/
 CREATE DATABASE mis_rutinas;
 USE mis_rutinas;
 
@@ -13,19 +12,25 @@ CREATE TABLE usuario(
     fec_ing date,
     IMC float
 );
-//*Arias Guillermo*/
+
+CREATE TABLE rutina(
+	id_rutina INT primary key not null,
+    nombre date
+);
+
 CREATE TABLE rut_usuario(
 	id_rut_usuario INT primary key not null,
     rut_id INT,
     usuario_id INT,
     rutina_id_rutina INT, 
-    constraint  fk_uid foreign key (usuario_id) references usuario (id_user),
-    constraint  fk_rir foreign key (rutina_id_rutina) references rutina (id_rutina)
+    constraint  fk_uid foreign key (usuario_id) references usuario(id_user),
+    constraint  fk_rir foreign key (rutina_id_rutina) references rutina(id_rutina)
 );
 
-CREATE TABLE rutina(
-	id_rutina INT primary key not null,
-    nombre date
+CREATE TABLE ejercicio(
+	id_ejerc INT primary key not null,
+	nombre VARCHAR(45),
+	zona_cuerpo VARCHAR(45)
 );
 
 CREATE TABLE rut_ejercicio(
@@ -36,42 +41,23 @@ CREATE TABLE rut_ejercicio(
     descanso varchar(45),
     rutina_id INT,
     ejercicio_id INT,
-    constraint fk_ri foreign key (rutina_id) references rutina (id_rutina),
-    constraint fk_ei foreign key (ejercicio_id) references ejercicio (id_ejerc)
-);
-
-/*Andrés*/
-CREATE TABLE ejercicio(
-	id_ejerc INT primary key not null,
-	nombre VARCHAR(45),
-	zona_cuerpo VARCHAR(45)
+    constraint fk_ri foreign key (rutina_id) references rutina(id_rutina),
+    constraint fk_ei foreign key (ejercicio_id) references ejercicio(id_ejerc)
 );
 
 CREATE TABLE logro(
 	id_logro INT primary key not null,
 	medalla VARCHAR(45),
-	constraint fk_rutina_id foreign key (id_rutina) references rutina(id_rutina)
+    fk_id_rutina INT,
+	FOREIGN KEY (fk_id_rutina) references rutina(id_rutina)
 );
 
 CREATE TABLE plan_alimentacion(
 	id_plan INT primary key not null,
 	archivo VARCHAR (45),
 	req_agua VARCHAR(45),
-	constraint fk_id_user foreign key (id_user) references usuario(id_user)
-);
-
-/*Horacio*/
-CREATE TABLE calendario(
-	id_calen INT primary key not null,
-	fecha DATE,
-	hora VARCHAR(45),
-	descripcion VARCHAR(45),
-	historial_user_id INT,
-	rut_ejerc_id INT,
-	logro_rutina_id INT,
-	constraint fk_hui foreign key (historial_user_id) references historial(id_historial),
-	constraint fk_rei foreign key (rut_ejerc_id) references rut_ejerc(id_rut_ejerc),
-	constraint fk_lri foreign key (logro_rutina_id) references logro(id_logro)
+    fk_id_user int,
+	foreign key (fk_id_user) references usuario(id_user)
 );
 
 CREATE TABLE historial(
@@ -81,4 +67,17 @@ CREATE TABLE historial(
 	IMC float,
 	user_id INT, 
 	constraint fk_ui foreign key (user_id) references usuario(id_user)
+);
+
+CREATE TABLE calendario(
+	id_calen INT primary key not null,
+	fecha DATE,
+	hora VARCHAR(45),
+	descripcion VARCHAR(45),
+	historial_user_id INT,
+	rut_ejerc_id INT,
+	logro_rutina_id INT,
+	constraint fk_hui foreign key (historial_user_id) references historial(id_historial),
+	constraint fk_rei foreign key (rut_ejerc_id) references rut_ejercicio(id_rut_ejerc),
+	constraint fk_lri foreign key (logro_rutina_id) references logro(id_logro)
 );
