5b98d5316751300fb840562af18fbb40a600f72e
misterio07 <darioguille92@gmail.com>
2022-10-31 23:41:53 -0300

    correccion de validacion de formulario

--- frontend/View/js/script.js
+++ frontend/View/js/script.js
@@ -60,10 +60,10 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    nombre: /^[a-zA-ZÀ-ÿ\s]{4,40}$/,
     email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    asunto: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-    mensaje: /^[a-zA-ZÀ-ÿ\s]{1,40}$/
+    asunto: /^[a-zA-ZÀ-ÿ\s]{6,40}$/,
+    mensaje: /^[a-zA-ZÀ-ÿ\s]{6,40}$/
 }
 
 const campos = {
