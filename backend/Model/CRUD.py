2b27885e62f5450629dd718f9a6899cb7faeeab0
horacioequiroga <horacioequiroga2020@gmail.com>
2022-11-13 23:18:22 -0300

    se Sube CRUD primeros comandos

---
+++ backend/Model/CRUD.py
@@ -0,0 +1,46 @@
+import mysql.connector
+
+class Conectar():
+
+    def __init__(self) -> None:
+        try:
+            self.conexion = mysql.connector.connect(
+                host = 'localhost',
+                port = 3306,
+                user = 'root',
+                password = 'root',
+                db = 'mis_rutinas'
+
+            )
+        except mysql.connector.Error as descripcionError:
+            print("¡No se conectó!",descripcionError)
+
+#PRIMERA OPERACIÓN DEL CRUD: CREATE O INSERT.
+    def InsertarValor(self,nombre, apellido, fec_nac, nom_user, email, password, fecha_ing):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sentenciaSQL= "INSERT INTO usuario VALUES(%s,%s,%s,%s,%s,%s,%s)"
+                data= (nombre, apellido, fec_nac, nom_user, email, password, fecha_ing)
+                cursor.execute(sentenciaSQL,data)
+                self.conexion.commit()
+                self.conexion.close()
+
+            except:
+                print("No se pudo concectar a la base de datos")
+   
+#SEGUNDA OPERACION DEL CRUD: READ O LEER
+    def BuscarObjeto(self, dato):        
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sentenciaSQL= "SELECT * FROM usuario WHERE nombre LIKE '%s' "
+                data= (dato)
+                cursor.execute(sentenciaSQL,data)
+                resultadoREAD = cursor.fetchall()
+                self.conexion.close()
+                return resultadoREAD
+
+            except:
+                print("No se pudo concectar a la base de datos")
+ 
