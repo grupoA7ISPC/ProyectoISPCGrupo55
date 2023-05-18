baeb59203833b4c122aceab5ce302f1219160b3a
Carina Payleman <rociocarina2@gmail.com>
2023-05-17 22:05:42 -0300

    Añade HttpModule y petición get de suscripcion

--- frontend/mis-rutinas/src/app/app.module.ts
+++ frontend/mis-rutinas/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { PagesModule } from './pages/pages.module';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { LayoutModule } from './layout/layout.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,8 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
-    LayoutModule
+    LayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/mis-rutinas/src/app/service/suscripcion.service.ts
+++ frontend/mis-rutinas/src/app/service/suscripcion.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuscripcionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private urlApi: string = 'http://localhost:3000/suscripciones';
+
+  getAllSubscriptions() {
+    return this.http.get(this.urlApi);
+  }
+
+  getSubscription(id: number) {
+    return this.http.get(`${this.urlApi}/${id}`);
+  }
+
 }
