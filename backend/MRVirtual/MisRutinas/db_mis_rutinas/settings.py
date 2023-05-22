54a6d524d19fd61c7066ded979e8884307800d6d
Carina Payleman <rociocarina2@gmail.com>
2023-05-22 01:38:25 -0300

    Renombra carpeta db

--- backend/MRVirtual/MisRutinas/db_mis_rutinas/settings.py
+++ backend/MRVirtual/MisRutinas/db_mis_rutinas/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MisRutinas'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +78,19 @@ WSGI_APPLICATION = 'MisRutinas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mis_rutinas',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'sql_mode': 'traditional'
+#         }
+#     }
+# }
 
 
 # Password validation
