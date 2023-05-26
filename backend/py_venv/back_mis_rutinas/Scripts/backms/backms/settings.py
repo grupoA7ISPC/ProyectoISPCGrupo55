4452b0237e7ea341a286b6dd2608f537fd5ad808
andresito-luna <andresluna007@gmail.com>
2023-05-26 20:20:11 -0300

    conexion con bd aws

--- backend/py_venv/back_mis_rutinas/Scripts/backms/backms/settings.py
+++ backend/py_venv/back_mis_rutinas/Scripts/backms/backms/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'backms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ispc',
+        'USER': 'andres',
+        'PASSWORD': '10366301',
+        'HOST': 'alu-ispc.clfcnl4qjyxz.sa-east-1.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
