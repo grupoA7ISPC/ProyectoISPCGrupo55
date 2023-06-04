07cf58bac761a53e12e55c3a051e9157c48f5884
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-06-04 19:07:44 -0300

    Creado constructor, FormBuilder y binding del registerForm

--- frontend/mis-rutinas/src/app/pages/registro/registro.component.ts
+++ frontend/mis-rutinas/src/app/pages/registro/registro.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
+
 export class RegistroComponent {
-
-
+  public registerForm;
+  constructor(private formBuilder : FormBuilder){
+    this.registerForm = this.formBuilder.group({
+      nombre : ['',[]],
+      apellido : ['',[]],
+      usuario : ['',[]],
+      email : ['',[]],
+      password1 :['',[]],
+      password2 : ['',[]],
+      fecha : ['',[]],
+      checkbox : ['',[]]
+    })
+  }
+  sendForm(event:Event){
+    event.preventDefault();
+    this.registerForm.valid ? alert("Enviando al servidor...") : this.registerForm.markAllAsTouched();
+  }
 }
+
+
