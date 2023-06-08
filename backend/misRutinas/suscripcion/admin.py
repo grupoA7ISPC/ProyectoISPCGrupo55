06ba6ddb4ee1187249a837d596bfac6206100ce5
fede9087 <fede9087@gmail.com>
2023-06-08 18:55:57 -0300

    Agrego tabla de suscripciones , agregada en el panel de admin

--- backend/misRutinas/suscripcion/admin.py
+++ backend/misRutinas/suscripcion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class SuscripcionAdmin(admin.ModelAdmin):
+    list_display=list([field.name for field in Suscripcion._meta.get_fields()])
+
+admin.site.register(Suscripcion, SuscripcionAdmin)
--- backend/misRutinas/suscripcion/models.py
+++ backend/misRutinas/suscripcion/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Suscripcion(models.Model):
+  id_sub = models.AutoField(primary_key=True)
+  nombre = models.TextField(max_length=50)
+  descripcion = models.TextField(max_length=370)
+  price = models.FloatField()
+  dur_dias = models.IntegerField()
+
+  class Meta:
+    db_table="Suscripcion"
+    verbose_name="suscripciones"
+    verbose_name_plural="suscripciones"
+
+  def __unicode__(self):
+    return self.nombre
+    
+  def __str__(self):
+    return self.nombre
