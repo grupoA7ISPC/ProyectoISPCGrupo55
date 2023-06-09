a2e9d9a2cfbc8aaf3358d0119cde6b1115d50e50
estebanmartins <estebanmartins25@gmail.com>
2023-06-09 19:37:23 -0300

    Cree tabla Alimentacion,se enlazo con Admin

--- backend/misRutinas/usuarios/admin.py
+++ backend/misRutinas/usuarios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class AlimentacionAdmin(admin.ModelAdmin):
+    list_display = list([field.name for field in Alimentacion._meta.get_fields()])
+
+admin.site.register(Alimentacion, AlimentacionAdmin)
--- backend/misRutinas/usuarios/models.py
+++ backend/misRutinas/usuarios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Alimentacion(models.Model):
+  id_alim = models.AutoField(primary_key=True)
+  title = models.CharField(max_length=50)
+  descripcion = models.TextField(max_length=600) 
+  summary = models.TextField(max_length=100) 
+  class Meta:
+    db_table = 'Alimentacion'
+    verbose_name = ('tips de alimentacion')
+    verbose_name_plural = ('Alimentaciones')
+    
+  def __unicode__(self):
+    return self.title
+  def __str__(self):
+    return self.title
