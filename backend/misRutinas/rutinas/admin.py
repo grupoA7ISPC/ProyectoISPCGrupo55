18a78383c821afac572f93e2217adacefb838f68
Carina Payleman <rociocarina2@gmail.com>
2023-06-10 16:50:12 -0300

    Vuelve a indexar app rutinas al proy, elimina 3 tablas iniciales, crea tabla Set en Admin. Elimina y cambia migraciones

--- backend/misRutinas/rutinas/admin.py
+++ backend/misRutinas/rutinas/admin.py
@@ -3,17 +3,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ("id_User", "name", "last_name", "birthday", "username", "email", "password", "created_at", "imc")
+class SetAdmin(admin.ModelAdmin):
+    list_display = ("id_set", "serie", "repeticion", "duracion", "descanso")
 
-admin.site.register(Usuario, UsuarioAdmin)
-
-class PagoAdmin(admin.ModelAdmin):
-    list_display = ("id_pago","fecha","periodo")
-
-admin.site.register(Pago, PagoAdmin)
-
-class RutinaAdmin(admin.ModelAdmin):
-    list_display = ("id_rutina", "nombre", "serie", "repeticion", "duracion", "descanso")
-
-admin.site.register(Rutina, RutinaAdmin)
+admin.site.register(Set, SetAdmin)
