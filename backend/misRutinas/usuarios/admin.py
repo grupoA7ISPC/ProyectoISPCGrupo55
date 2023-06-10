3a14c1046f6550065a21088ec7ffc84f300e53a9
Carina Payleman <rociocarina2@gmail.com>
2023-06-10 01:02:59 -0300

    Crea tabla Usuarios, enlaza con panel admin y tabla Alimentacion (no con Suscripcion)

--- backend/misRutinas/usuarios/admin.py
+++ backend/misRutinas/usuarios/admin.py
@@ -4,6 +4,11 @@ from .models import *
 
 # Register your models here.
 class AlimentacionAdmin(admin.ModelAdmin):
-    list_display = list([field.name for field in Alimentacion._meta.get_fields()])
+    list_display = ('id_alim', 'title', 'descripcion', 'summary')
 
 admin.site.register(Alimentacion, AlimentacionAdmin)
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id_user', 'id_alim', 'created_at', 'nombre', 'apellido', 'username', 'fec_nac', 'email', 'password', 'peso', 'altura', 'imc')
+    
+admin.site.register(Usuario, UsuarioAdmin)
--- backend/misRutinas/usuarios/models.py
+++ backend/misRutinas/usuarios/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password
+# from suscripcion.models import Suscripcion
 
 # Create your models here.
-
 class Alimentacion(models.Model):
   id_alim = models.AutoField(primary_key=True)
   title = models.CharField(max_length=50)
@@ -16,3 +18,35 @@ class Alimentacion(models.Model):
     return self.title
   def __str__(self):
     return self.title
+
+class Usuario(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    id_alim = models.ForeignKey(Alimentacion, on_delete=models.CASCADE, default=1)
+    # id_sub = models.ForeignKey(Suscripcion, on_delete=models.CASCADE, default=1)
+    created_at = models.DateTimeField(default=timezone.now)
+    nombre = models.CharField(max_length=45)
+    apellido = models.CharField(max_length=45)
+    username = models.CharField(max_length=16, unique=True)
+    fec_nac = models.DateField()
+    email = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=16)
+    peso = models.FloatField(blank=True, default=0)
+    altura = models.FloatField(blank=True, default=0)
+    imc = models.FloatField( default=0, blank=True)
+
+    class Meta:
+      db_table="Usuario"
+      verbose_name="Usuario de MisRutinas"
+      verbose_name_plural="Usuarios"
+
+    def __unicode__(self):
+      return self.nombre
+    
+    def __str__(self):
+       return self.nombre
+  
+    def set_password(self, raw_password):
+      self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+      self.check_password(raw_password, self.password)
