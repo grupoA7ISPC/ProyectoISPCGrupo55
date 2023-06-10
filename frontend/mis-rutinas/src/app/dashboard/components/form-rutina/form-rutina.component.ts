a637efbdb2ee39f381608f39e6866ca3958a4dbb
VeroTorres <verofxianeca@gmail.com>
2023-06-09 23:56:15 -0300

    creacion de componente formRutina

---
+++ frontend/mis-rutinas/src/app/dashboard/components/form-rutina/form-rutina.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-rutina',
+  templateUrl: './form-rutina.component.html',
+  styleUrls: ['./form-rutina.component.css']
+})
+export class FormRutinaComponent {
+  titulo = 'Formulario';
+
+}
