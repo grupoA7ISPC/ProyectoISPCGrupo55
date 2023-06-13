0abd932abe8c332a446352833b695a9c46799023
Carina Payleman <rociocarina2@gmail.com>
2023-06-13 17:23:50 -0300

    Configura settings para trabajar con CORS y rest framework

--- backend/misRutinas/misRutinas/settings/settingsBase.py
+++ backend/misRutinas/misRutinas/settings/settingsBase.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rutinas',
     'suscripcion',
-    'usuarios'
+    'usuarios',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'misRutinas.urls'
@@ -122,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost',
+]
