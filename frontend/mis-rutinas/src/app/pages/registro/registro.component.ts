2edb4069a9329aea6522ee2aa87ddee58900d260
estebanmartins <estebanmartins25@gmail.com>
2023-06-19 16:02:32 -0300

    Se realiza validaciones en registro de max-min lenght de nombre apellido,email valido y fecha

--- frontend/mis-rutinas/src/app/pages/registro/registro.component.ts
+++ frontend/mis-rutinas/src/app/pages/registro/registro.component.ts
@@ -15,13 +15,13 @@ export class RegistroComponent {
   
   constructor(private formBuilder: FormBuilder, private UsuarioService: UsuarioService, private router: Router) {
     this.registerForm = this.formBuilder.group({
-      nombre : ['',[Validators.required]],
-      apellido : ['',[Validators.required]],
+      nombre: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
+      apellido : ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
       usuario : ['',[Validators.required]],
       email : ['',[Validators.required, Validators.email]],
       password1: ['', Validators.required],
       password2: ['', Validators.required],
-      fecha : ['',[Validators.required]],
+      fecha: ['', [Validators.required, this.fechaValidator]],
       checkbox : ['',[Validators.required, Validators.requiredTrue]]
     }, { validator: this.passwordMatchValidator('password1', 'password2')});
   }
@@ -39,28 +39,27 @@ export class RegistroComponent {
     };
   }
 
-  dateValidator(fechakey: any):ValidatorFn{
-    return (control: AbstractControl): ValidationErrors | null =>{
-      const fecha = control.get(fechakey)?.value;
-      let fechaMinima= new Date('01/01/1900');
-      const fechaNacimiento = new Date (fecha);
-      let fechaActual = new Date();
-      const diferenciaFechas = new Date(
-          fechaNacimiento.getUTCFullYear() + 13,
-          fechaNacimiento.getUTCMonth(),
-          fechaNacimiento.getUTCDate()
-      );
-
-      if (new Date(fecha) > new Date() || new Date(fecha) < fechaMinima) {
-        return { 'fechaInvalida': 'Ingrese una fecha válida' }
-      } else if (diferenciaFechas >= fechaActual) {
-        return { 'fechaMinima': 'Debes tener al menos 13 años' }
-      }
-
-      return null;
+  
+  fechaValidator(control: FormControl) {
+    const fechaNacimiento = new Date(control.value);
+    const fechaMinima = new Date();
+    fechaMinima.setFullYear(fechaMinima.getFullYear() - 13); // Restar 13 años a la fecha actual
+  
+    if (isNaN(fechaNacimiento.getTime())) {
+      return { fechaInvalida: true }; // Retorna una clave personalizada para el error
     }
+  
+    if (fechaNacimiento > fechaMinima) {
+      return { fechaMinima: true }; // Retorna una clave personalizada para el error
+    }
+  
+    if (fechaNacimiento.getFullYear() < fechaMinima.getFullYear() - 99) {
+      return { fechaInvalida: true }; // Retorna una clave personalizada para el error
+    }
+  
+    return null; // Retorna null si la validación es exitosa
   }
-
+  
  /* sendForm(event:Event){
     event.preventDefault();
     this.registerForm.valid ? alert("Enviando al servidor...") : this.registerForm.markAllAsTouched();
