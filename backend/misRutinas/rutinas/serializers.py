b1cc3f31b406e45b4c2a610e367118008d2cb8e0
Carina Payleman <rociocarina2@gmail.com>
2023-06-28 00:07:53 -0300

    Modifica view de /setrutinas: muestra objetos ejercicio

--- backend/misRutinas/rutinas/serializers.py
+++ backend/misRutinas/rutinas/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Rutina, Set, SetRutina
+from .models import Rutina, Set, SetRutina, Ejercicio
+
+class EjercicioSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Ejercicio
+        fields = "__all__"
 
 class RutinaSerializer (serializers.ModelSerializer):
     class Meta:
@@ -7,9 +12,12 @@ class RutinaSerializer (serializers.ModelSerializer):
         fields = "__all__"
         
 class SetSerializer (serializers.ModelSerializer):
+    fk_ejer = EjercicioSerializer(read_only=True)
+    
     class Meta:
         model = Set
         fields = "__all__"
+        depth = 1
 
 class SetRutinaSerializer(serializers.ModelSerializer):
     fk_rut = RutinaSerializer(read_only=True)
