225bb2e5455bd40fb3b4265d41bf1f23fe19243c
Carina Payleman <rociocarina2@gmail.com>
2023-06-28 18:27:00 -0300

    Envía datos necesarios al frontend tras login

--- backend/misRutinas/login/views.py
+++ backend/misRutinas/login/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate
 from rutinas.models import Rutina
+from suscripcion.models import Factura, Pago
 from rest_framework import serializers
 from django.core.serializers import serialize
 
@@ -19,12 +20,12 @@ def login_view(request):
         user = Usuario.check_credentials(email, password)
         print("User: ", user)
         if user is not None:       
-            rutinas = Rutina.objects.filter(fk_user=user.id_user) 
-            
+            # Se envían los datos precisados para el MVP:
             user_data = {
+                'id': user.id_user,
+                'id_sub': user.id_sub.id_sub, # No es iterable
                 'username': user.username,
                 'email': user.email,
-                'id' : user.id_user,
                 'nombre' : user.nombre,
                 'apellido' : user.apellido,
                 'fec_nac' : user.fec_nac, 
@@ -33,17 +34,21 @@ def login_view(request):
                 'imc' : user.imc
             }
             
+            rutinas = Rutina.objects.filter(fk_user=user.id_user)             
             rutinas_data = serialize('json', rutinas)
             
+            factura = Factura.objects.filter(fk_id_user=user.id_user)
+            factura_data = serialize('json', factura)
+                        
             data = {
                 'user': user_data,
-                'rutinas': rutinas_data
+                'rutinas': rutinas_data,
+                'factura': factura_data
             }
             
             print(data)
             return JsonResponse(data)
         else:
-            # Usuario o contraseña inválidos
             return JsonResponse({'error': 'Credenciales inválidas'}, status=400)
             
     return JsonResponse({'error': 'Metodo no permitido'}, status=405)
