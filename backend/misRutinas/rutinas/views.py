f4798653d0d1af8629c9d8ea322d2a2cbd81977d
Carina Payleman <rociocarina2@gmail.com>
2023-06-27 23:48:28 -0300

    Crea filtro para buscar por fk_user en setrutinas

--- backend/misRutinas/rutinas/views.py
+++ backend/misRutinas/rutinas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+
 from .serializers import RutinaSerializer, SetRutinaSerializer
 from .models import Rutina, SetRutina
 
@@ -11,5 +12,11 @@ class RutinasViewSet(viewsets.ModelViewSet):
 class SetRutinasViewSet(viewsets.ModelViewSet):
     queryset = SetRutina.objects.all()
     serializer_class = SetRutinaSerializer    
+    filter_backends = [filters.SearchFilter]
     
-# SetRutina.objects.get(pk=id) 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_param = self.request.query_params.get('search', None)
+        if search_param is not None:
+            queryset = queryset.filter(fk_rut__fk_user=search_param)
+        return queryset
